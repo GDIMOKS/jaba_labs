833c8e06121a15be9fb5487d545e3fec5e5e58b8
Dmitriy Fedorov <>
2024-05-29 18:32:29 +0300

    попытка упрощения

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -1,9 +1,24 @@
 package lab3;
 
+import java.util.HashMap;
 import java.util.Map;
+
+//import static lab3.Simplifier.simplify;
 
 public class Main {
     public static void main(String[] args) {
+        Lab3();
+    }
+
+    public static void Lab3() {
+        Task1();
+        System.out.println("\n////////////////////////////////////\n");
+//        Task2();
+//        System.out.println("\n////////////////////////////////////\n");
+//        Task3();
+    }
+
+    public static void Task1() {
         Parser parser = new Parser();
         Node f = parser.parse("x + 2 * (y - -3 + z*z) + cos(0)");
         Map<String, Double> variables = Map.of("x", 2.0, "y", 3.0, "z", 4.0);
@@ -12,7 +27,28 @@ public class Main {
         Node f2 = parser.parse("3 + 5 * 2");
         System.out.println(f2.evaluate(Map.of()));
 
-        Node f3 = parser.parse("pow(25, -1)");
+        Node f3 = parser.parse("cos(0)");
         System.out.println(f3.evaluate(Map.of()));
+
+        Node f4 = parser.parse("-5 + 3");
+        System.out.println(f4.evaluate(Map.of()));
     }
+
+    public static void Task2() {
+        Parser parser = new Parser();
+        // Исходное выражение
+        Node f = parser.parse("(x + 1) * (x + 1)");
+        System.out.println(f.evaluate(Map.of("x", 1.0)));
+        // Упрощенное выражение с удаленными дубликатами подвыражений
+//        Node g = simplify(f);
+
+        // Вывод результата упрощенного выражения
+//        System.out.println(g.evaluate(Map.of("x", 1.0)));
+    }
+
+    public static void Task3() {
+
+
+    }
+
 }
--- src/lab3/Simplifier.java
+++ src/lab3/Simplifier.java
@@ -1,41 +1,79 @@
-package lab3;
-import java.util.HashMap;
-import java.util.Map;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Simplifier {
-
-    public static Node simplify(Node node) {
-        Map<String, Node> uniqueNodes = new HashMap<>();
-        return simplifyNode(node, uniqueNodes);
-    }
-
-    private static Node simplifyNode(Node node, Map<String, Node> uniqueNodes) {
-        if (node instanceof BinaryOperationNode) {
-            BinaryOperationNode binaryNode = (BinaryOperationNode) node;
-            Node left = simplifyNode(binaryNode.getLeft(), uniqueNodes);
-            Node right = simplifyNode(binaryNode.getRight(), uniqueNodes);
-            return new BinaryOperationNode(left, right, binaryNode.getOperator());
-        } else if (node instanceof UnaryOperationNode) {
-            UnaryOperationNode unaryNode = (UnaryOperationNode) node;
-            Node child = simplifyNode(unaryNode.getOperand(), uniqueNodes);
-            return new UnaryOperationNode(child, unaryNode.getOperator());
-        } else if (node instanceof FunctionNode) {
-            FunctionNode functionNode = (FunctionNode) node;
-            Node[] arguments = new Node[functionNode.getArguments().length];
-            for (int i = 0; i < functionNode.getArguments().length; i++) {
-                arguments[i] = simplifyNode(functionNode.getArguments()[i], uniqueNodes);
-            }
-            return new FunctionNode(functionNode.getFunctionName(), arguments);
-        } else if (node instanceof VariableNode || node instanceof ConstantNode || node instanceof NumberNode) {
-            String key = node.toString();
-            if (!uniqueNodes.containsKey(key)) {
-                uniqueNodes.put(key, node);
-            }
-            return uniqueNodes.get(key);
-        }
-        return null;
-    }
-}
+//package lab3;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Simplifier {
+//
+//    // Метод упрощения функции
+//    public static Node simplify(Node node) {
+//        // Создаем карту для отслеживания уникальных узлов
+//        Map<String, Node> uniqueNodes = new HashMap<>();
+//        // Вызываем вспомогательный метод для упрощения узла
+//        return simplifyNode(node, uniqueNodes);
+//    }
+//
+//    private static Node simplifyNode(Node node, Map<String, Node> uniqueNodes, Map<String, Double> variableValues) {
+//        // Сначала проверяем, если узел уже упрощенный, то возвращаем его
+//        if (uniqueNodes.containsKey(node.toString())) {
+//            return uniqueNodes.get(node.toString());
+//        }
+//
+//        // Если узел является бинарной операцией
+//        if (node instanceof BinaryOperationNode) {
+//            // Получаем ссылку на узел бинарной операции
+//            BinaryOperationNode binaryNode = (BinaryOperationNode) node;
+//            // Рекурсивно упрощаем левый и правый дочерние узлы
+//            Node left = simplifyNode(binaryNode.getLeft(), uniqueNodes, variableValues);
+//            Node right = simplifyNode(binaryNode.getRight(), uniqueNodes, variableValues);
+//            // Создаем новый узел бинарной операции с упрощенными дочерними узлами
+//            BinaryOperationNode simplifiedNode = new BinaryOperationNode(left, right, binaryNode.getOperator());
+//            // Добавляем узел в карту уникальных узлов
+//            uniqueNodes.put(simplifiedNode.toString(), simplifiedNode);
+//            // Возвращаем упрощенный узел
+//            return simplifiedNode;
+//        }
+//        // Если узел является унарной операцией
+//        else if (node instanceof UnaryOperationNode) {
+//            // Получаем ссылку на узел унарной операции
+//            UnaryOperationNode unaryNode = (UnaryOperationNode) node;
+//            // Рекурсивно упрощаем дочерний узел
+//            Node operand = simplifyNode(unaryNode.getOperand(), uniqueNodes, variableValues);
+//            // Создаем новый узел унарной операции с упрощенным дочерним узлом
+//            UnaryOperationNode simplifiedNode = new UnaryOperationNode(operand, unaryNode.getOperator());
+//            // Добавляем узел в карту уникальных узлов
+//            uniqueNodes.put(simplifiedNode.toString(), simplifiedNode);
+//            // Возвращаем упрощенный узел
+//            return simplifiedNode;
+//        }
+//        // Если узел является функцией
+//        else if (node instanceof FunctionNode) {
+//            // Получаем ссылку на узел функции
+//            FunctionNode functionNode = (FunctionNode) node;
+//            // Рекурсивно упрощаем аргументы функции
+//            Node[] arguments = new Node[functionNode.getArguments().length];
+//            for (int i = 0; i < functionNode.getArguments().length; i++) {
+//                arguments[i] = simplifyNode(functionNode.getArguments()[i], uniqueNodes, variableValues);
+//            }
+//            // Создаем новый узел функции с упрощенными аргументами
+//            FunctionNode simplifiedNode = new FunctionNode(functionNode.getFunctionName(), arguments);
+//            // Добавляем узел в карту уникальных узлов
+//            uniqueNodes.put(simplifiedNode.toString(), simplifiedNode);
+//            // Возвращаем упрощенный узел
+//            return simplifiedNode;
+//        }
+//        // Если узел является переменной, константой или числом
+//        else if (node instanceof VariableNode || node instanceof ConstantNode || node instanceof NumberNode) {
+//            // Для переменных и констант получаем их значения из карты
+//            if (variableValues.containsKey(node.toString())) {
+//                return new NumberNode(variableValues.get(node.toString()));
+//            } else {
+//                // Если значения нет, просто добавляем узел в карту уникальных узлов
+//                uniqueNodes.put(node.toString(), node);
+//                // Возвращаем оригинальный узел
+//                return node;
+//            }
+//        }
+//        // Если узел неизвестного типа, возвращаем null
+//        return null;
+//    }
+//}
